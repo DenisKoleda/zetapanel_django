cdb27abf5a9eedb72760e7a97655fa2acd0ebc25
Denis Koleda <d.koleda@emergingtravel.com>
2023-11-03 01:16:35 +0400

    переделка действий

--- app/task/models.py
+++ app/task/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Task(models.Model):
     # Django автоматически создает ID-поле, если иное не указано
-    date = models.DateTimeField("date published")
+    date = models.DateField("date published")
     author = models.CharField(max_length=200)
     ticket = models.CharField(max_length=200)
     ticket_comment = models.TextField()  # Это поле предназначено для длинных строк текста
     priority = models.CharField(max_length=200)
     status = models.CharField(max_length=200)
     executor = models.CharField(max_length=200)
-    deadline = models.DateTimeField("deadline", blank=True, null=True)
+    deadline = models.DateField("deadline", blank=True, null=True)
 
     # Добавьте любые другие поля, необходимые для вашего приложения
 

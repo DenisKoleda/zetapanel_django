0ff583c519e0d171e81d25881f247c15de11d4e1
Denis Koleda <d.koleda@emergingtravel.com>
2023-11-09 03:51:27 +0400

    добавлены комментарии + правки

--- app/task/admin.py
+++ app/task/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from .models import Task, Comment
+from .models import Task, Comment, FileAttachment
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('date', 'author', 'ticket', 'priority', 'status', 'executor', 'deadline')
+    list_display = ('id', 'date', 'author', 'ticket', 'priority', 'status', 'executor', 'deadline')
     list_filter = ('date', 'author', 'priority', 'status', 'executor')
     search_fields = ['author', 'ticket', 'ticket_comment', 'priority', 'status', 'executor']
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'task', 'author', 'content', 'created_at')
+    list_filter = ('task', 'author', 'created_at')
+    search_fields = ['author', 'content']
+    
+class FileAttachmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'task', 'author', 'file', 'created_at')
+    list_filter = ('task', 'author', 'created_at')
+    search_fields = ['author', 'file']
+
 
 admin.site.register(Task, TaskAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(FileAttachment, FileAttachmentAdmin)
--- app/task/models.py
+++ app/task/models.py
@@ -1,29 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Task(models.Model):
-    # Django автоматически создает ID-поле, если иное не указано
-    date = models.DateField("date published")
-    author = models.CharField(max_length=200)
-    ticket = models.CharField(max_length=200)
-    ticket_comment = models.TextField(blank=True, null=True)  # Это поле предназначено для длинных строк текста
-    priority = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
-    executor = models.CharField(max_length=200)
-    deadline = models.DateField("deadline", blank=True, null=True)
+    date = models.DateField("Дата публикации")
+    author = models.CharField("Автор", max_length=200)
+    ticket = models.CharField("Тикет", max_length=200)
+    ticket_comment = models.TextField("Комментарий к тикету", blank=True, null=True)
+    priority = models.CharField("Приоритет", max_length=200)
+    status = models.CharField("Статус", max_length=200)
+    executor = models.CharField("Исполнитель", max_length=200)
+    deadline = models.DateField("Срок", blank=True, null=True)
 
-    # Добавьте любые другие поля, необходимые для вашего приложения
-
-    # Опционально: добавьте метод __str__ для более читаемого представления объектов модели
     def __str__(self):
         return f"{self.author} - {self.ticket}"
 
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+        ordering = ['-id']  # Пример сортировки по умолчанию
+
 class Comment(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    task = models.ForeignKey(Task, verbose_name="Задача", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, verbose_name="Автор", on_delete=models.CASCADE)
+    content = models.TextField("Содержание")
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.author} on Task {self.task.id}"
+        return f"Комментарий от {self.author} к задаче {self.task.id}"
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ['-id']
+
+class FileAttachment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='attachments')
+    author = models.ForeignKey(User, verbose_name="Автор", on_delete=models.CASCADE)
+    file = models.FileField(verbose_name="Файл", upload_to='attachments/')
+    description = models.CharField("Описание", max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+    
+    def __str__(self):
+        return f"Вложение {self.file.name} к задаче {self.task.id}"
+    
+    class Meta:
+        verbose_name = "Вложение"
+        verbose_name_plural = "Вложения"
+        ordering = ['-id']
